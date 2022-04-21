e6a1af207fa7c917851267cea406a1e6a1b2ebcd
oscar <oscar.sanchez@autentia.com>
2022-04-21 11:40:10 +0200

    First comit

---
+++ src/main/java/com/example/deploy/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.deploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public String greeting() {
+
+        return "Answer";
+
+    }
+
+}
